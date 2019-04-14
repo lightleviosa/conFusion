61b3bcb50b0ccb9fbc2acd1b26420c6b32a8907c
Luo Aiai <lucia.ai@gmail.com>
2019-04-14 11:29:56 -0400

    Angular Router Basics
    :100644 100644 fb9b67b 632117d M	.ftpconfig
    :000000 100644 0000000 17e2a33 A	src/app/about/about.component.html
    :000000 100644 0000000 e69de29 A	src/app/about/about.component.scss
    :000000 100644 0000000 6b77344 A	src/app/about/about.component.spec.ts
    :000000 100644 0000000 9a3c389 A	src/app/about/about.component.ts
    :100644 000000 d425c6f 0000000 D	src/app/app-routing.module.ts
    :000000 100644 0000000 6f22b49 A	src/app/app-routing/app-routing.module.ts
    :000000 100644 0000000 5ae6b44 A	src/app/app-routing/routes.ts
    :100644 100644 97f9830 2d46021 M	src/app/app.component.html
    :100644 100644 264d1a9 062150d M	src/app/app.module.ts
    :000000 100644 0000000 0be0885 A	src/app/contact/contact.component.html
    :000000 100644 0000000 e69de29 A	src/app/contact/contact.component.scss
    :000000 100644 0000000 427633e A	src/app/contact/contact.component.spec.ts
    :000000 100644 0000000 7f6bb1e A	src/app/contact/contact.component.ts
    :100644 100644 db32dcf 4d18a27 M	src/app/header/header.component.html
    :000000 100644 0000000 afc16a3 A	src/app/home/home.component.html
    :000000 100644 0000000 e69de29 A	src/app/home/home.component.scss
    :000000 100644 0000000 490e81b A	src/app/home/home.component.spec.ts
    :000000 100644 0000000 f56c8c1 A	src/app/home/home.component.ts

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+// components that are parts of the routing
+import { MenuComponent } from '../menu/menu.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+
+//TODO about and contact
+export const routes: Routes = [
+  { path: 'home',  component: HomeComponent },
+  { path: 'menu',     component: MenuComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
